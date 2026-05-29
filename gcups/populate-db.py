342977d9541f712a29792dad022d2a98c2240c53
Francesco Trapani <franky_92@live.it>
2026-05-29 16:31:45 +0200

    Fix GCUPS runtime database path

--- gcups/populate-db.py
+++ gcups/populate-db.py
@@ -8,6 +8,6 @@ with open("db.txt", "r") as f:
 
 data = [eval(line) for line in db_txt]
 
-db = plyvel.DB("/opt/gcups/db/gcups-rxdb-1-settings", create_if_missing=True)
+db = plyvel.DB("/home/gcups/.config/gcups/db/gcups-rxdb-1-settings", create_if_missing=True)
 for key, value in data:
     db.put(key, value)
